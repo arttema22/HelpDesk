f8c590f25d438b116574bfa4d3a46f3a1780168e
Artem Gusev <arttema@mail.ru>
2022-10-13 02:05:08 +0300

    Add adminlte

---
+++ resources/views/admin/role.blade.php
@@ -0,0 +1,9 @@
+@extends('admin.layouts.admin')
+
+@section('title')Админка@endsection
+
+@section('description')Прием обращений и заявок на оказание услуг.@endsection
+
+@section('content')
+Админка
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ServiceController;
 use App\Http\Controllers\TicketController;
+use App\Http\Controllers\Admin\RoleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,3 +26,5 @@ Route::post('/ticket', [TicketController::class, 'send'])->name('send');
 
 Route::get('/service/ticket', [ServiceController::class, 'index'])->name('service.ticket');
 Route::post('/service/ticket', [ServiceController::class, 'send'])->name('service.send');
+
+Route::get('/admin/role', [RoleController::class, 'index'])->name('role');
